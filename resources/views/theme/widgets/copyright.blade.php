2fb2bca4a16d33afdfddb407b75898b0dab0d7c2
Postbox <postbox@digitalbit.in>
2020-03-25 09:59:34 +0530

    updated postbox env file config

--- resources/views/theme/widgets/copyright.blade.php
+++ resources/views/theme/widgets/copyright.blade.php
@@ -2,7 +2,7 @@
     <div class="card-body text-left copyright-text">
         <div class="row">
             <div class="col-md-8 text-left">
-               Powered by <a target="_blank" title="Postbox" href="https://github.com/postboxcms/postbox" class="form-control">Postbox</a>
+               Powered by <a target="_blank" title="Postbox" href="https://github.com/postboxcms/postbox" class="copyright-link">Postbox</a>
             </div>
             <div class="col-md-4 text-right">
                 &copy; 2020
