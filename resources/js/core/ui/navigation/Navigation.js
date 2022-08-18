892c9c8780e65d85610fa37906d671bb53a85167
postboxcms <sanket@digitalbit.in>
2022-08-18 12:57:13 +0530

    Added collapsible sidenav menu

--- resources/js/core/ui/navigation/Navigation.js
+++ resources/js/core/ui/navigation/Navigation.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import ListSubheader from '@mui/material/ListSubheader';
+import Collapse from '@mui/material/Collapse';
 
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import BarChartIcon from '@mui/icons-material/BarChart';
-import LayersIcon from '@mui/icons-material/Layers';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -20,6 +24,10 @@ import iconList from '../../libs/icons';
 
 export const MainItems = (props) => {
     const [contentTypes, setContentTypes] = useState({ content_types: [] });
+    const [open,setOpen] = useState(false);
+    const collapsePanel = () => {
+        setOpen(!open);
+    }
 
     React.useEffect(() => {
         // fetch all content types
@@ -38,18 +46,31 @@ export const MainItems = (props) => {
                         <ListItemText primary="Dashboard" />
                     </ListItem>
                 </Link>
-                {contentTypes['content_types'].map((data) => {
-                    return (
-                        <Link to={data['slug']} key={data['id']}>
-                            <ListItem button>
-                                <ListItemIcon>
-                                    <FontAwesomeIcon size='lg' icon={data['icon']} />
-                                </ListItemIcon>
-                                <ListItemText primary={data['name']} />
-                            </ListItem>
-                        </Link>
-                    );
-                })}
+                <Link to="#">
+                    <ListItem button onClick={collapsePanel}>
+                        <ListItemIcon>
+                            <InventoryIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Content" />
+                        {open ? <ExpandLess /> : <ExpandMore />}
+                    </ListItem>
+                </Link>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        {contentTypes['content_types'].map((data) => {
+                            return (
+                                <Link to={data['slug']} key={data['id']}>
+                                    <ListItem button>
+                                        <ListItemIcon>
+                                            <FontAwesomeIcon size='lg' icon={data['icon']} />
+                                        </ListItemIcon>
+                                        <ListItemText primary={data['name']} />
+                                    </ListItem>
+                                </Link>
+                            );
+                        })}
+                    </List>
+                </Collapse>
             </div>
         </React.Fragment>
     )
