935b3c2ac1542f0f08ecf165b11e3b3e64858468
Sanket Raut <sanket@digitalbit.in>
2024-10-26 16:15:13 +0530

    UX - Side menu layout updates

--- resources/js/framework/app/theme.js
+++ resources/js/framework/app/theme.js
@@ -24,7 +24,7 @@ export const theme = createTheme({
     palette: {
         mode: 'light',
         primary: {
-            main: 'rgba(31, 0, 67, 0.8)',
+            main: 'rgba(31, 0, 67, 0.9)',
             dark: 'rgba(31, 0, 67, 1)'
         },
         plain: {
