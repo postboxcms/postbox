aee1a72959cfca5f088c0b6d7e25a5617585c0ca
Sanket Raut <sanket@digitalbit.in>
2025-01-11 10:59:57 +0530

    PLUGIN - Inspire package config

---
+++ config/inspire.php
@@ -0,0 +1,12 @@
+<?php
+/*
+* This file is part of the PostboxCMS\Inspire package.
+* (c) PostboxCMS <sanket@digitalbit.in>
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+return [
+    'base_url' => env('INSPIRE_BASE_URL', 'https://zenquotes.io'),
+    'api_endpoint' => env('INSPIRE_API_ENDPOINT', '/api/random'),
+];
