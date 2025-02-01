6ca9491df8c7f25fecaf838ac463994a2aff1799
Sanket Raut <sanket@digitalbit.in>
2025-02-01 11:47:07 +0000

    ARCH - modified postbox config

--- config/postbox.php
+++ config/postbox.php
@@ -1,12 +1,3 @@
 <?php
 // Postbox configuration setup
-
-return [
-    'database' => [
-        'models' => [
-            'posts' => \App\Models\Post::class,
-            'pages' => \App\Models\Page::class,
-            'users' => \App\Models\User::class,
-        ]
-    ]
-];
+return [];
