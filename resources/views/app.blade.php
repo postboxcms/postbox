cca571378620f94825b35e419c49be945ab43405
Sanket Raut <sanket@digitalbit.in>
2025-11-30 14:14:19 +1100

    chore: css loader updated

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,29 +1,29 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{csrf_token()}}">
 
-        <title>Postbox</title>
-        <link href="{{asset('css/app.css')}}" rel="stylesheet"/>
-        <link href="{{asset('css/theme.css')}}" rel="stylesheet"/>
-    </head>
-    <body>
-        <div id="app">
-            <div class="app-loader">
-                <div class="cube-wrapper">
-                    <div class="cube-folding">
-                        <span class="leaf1"></span>
-                        <span class="leaf2"></span>
-                        <span class="leaf3"></span>
-                        <span class="leaf4"></span>
-                    </div>
-                    <span class="loading" data-name="Postbox">Postbox is loading</span>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{csrf_token()}}">
+
+    <title>Postbox</title>
+    <link href="{{asset('css/app.css')}}" rel="stylesheet" />
+    <link href="{{asset('css/theme.css')}}" rel="stylesheet" />
+</head>
+
+<body>
+    <div id="app">
+        <div class="app-loader">
+            <div class="spinner-wrapper">
+                <div class="shadow"></div>
+                <div class="loader">
+                    <div class="inner-ball"></div>
                 </div>
             </div>
-            {{-- React code UI rendered here --}}
         </div>
-        <script src="{{asset('js/app.js')}}"></script>
-    </body>
+        {{-- React code UI rendered here --}}
+    </div>
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+
 </html>
