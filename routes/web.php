c28e3f59f68fb71ca3ff0b6d35fdee07d267d0fc
Sanket Raut <sanket@digitalbit.in>
2025-11-30 16:14:34 +1100

    chore: changed reference of ssr url

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::group(['prefix' => env('MIX_ADMIN_PREFIX', '/admin')], function () {
 });
 
 Route::get('/{any}', function (Settings $db) {
-    $response = Http::get('http://host.docker.internal:5172/server?url=' . request()->getRequestUri());
+    $response = Http::get(env('VITE_SSR_URL') . ':' . env('VITE_SSR_PORT') . '/server?url=' . request()->getRequestUri());
     $html = $response->body();
     $theme = $db->where('property', 'theme')->value('value');
     return view('web', ['html' => $html, 'theme' => $theme]);
