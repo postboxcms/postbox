a849f7a5c009c8474f76f83751c9dce9bce03714
Sanket Raut <sanket@digitalbit.in>
2026-01-04 12:05:43 +1100

    chore: updated refs

--- resources/js/framework/app/hooks/auth.js
+++ resources/js/framework/app/hooks/auth.js
@@ -1,21 +1,39 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 export const useAuth = () => {
-    const token = useSelector((state) => state.auth.token);
-    const dispatch = useDispatch();
+  const token = useSelector((state) => state.auth.token);
+  const dispatch = useDispatch();
 
-    const login = (token) => {
-        dispatch({ type: "LOGIN", payload: token });
-    };
+  const login = (token) => {
+    dispatch({ type: 'LOGIN', payload: token });
+  };
 
-    const logout = () => {
-        dispatch({ type: "LOGOUT" });
-    };
+  const logout = () => {
+    dispatch({ type: 'LOGOUT' });
+  };
 
-    return {
-        token,
-        login,
-        logout,
-    };
-}
+  return {
+    token,
+    login,
+    logout,
+  };
+};
+
+export const usePermissions = () => {
+  const hasNotification = (notification) =>
+    notification !== undefined && notification.message !== '';
+  const hasUserAuthenticated = (token) => token !== null && token !== undefined;
+  const hasAdminRoute = () =>
+    typeof window !== typeof undefined
+      ? window.location.href.includes('/admin')
+        ? true
+        : false
+      : false;
+
+  return {
+    hasNotification,
+    hasUserAuthenticated,
+    hasAdminRoute,
+  };
+};
