92a93cdfd50789fc9caa2192898daaef2d7affc7
Sanket Raut <sanket@digitalbit.in>
2026-05-10 09:19:18 +1000

    chore: reverted the fetching dep flag

--- resources/js/framework/website/components/Consumer.js
+++ resources/js/framework/website/components/Consumer.js
@@ -83,7 +83,7 @@ export const Consumer = ({ children, scroll, table, offset, limit }) => {
         setStart(start);
         setEnd(end);
       });
-  }, []);
+  }, [fetching]);
 
   return (
     <ConsumerContext.Provider value={{ consumer, fetching }}>{children}</ConsumerContext.Provider>
